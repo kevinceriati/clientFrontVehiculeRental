c22c55159094b7c10e66cd36f218b8f6fccbcf83
vozoul <vozoul@gmail.com>
2019-11-27 10:21:12 +0100

    add url apis

--- src/main/java/app/front/client/controller/MainController.java
+++ src/main/java/app/front/client/controller/MainController.java
@@ -1,16 +1,33 @@
 package app.front.client.controller;
 
+import app.front.client.model.Car;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Controller
 public class MainController {
 
+    @Value("${api.client}")
+    private String userBase;
+
+    @Value("${api.cars}")
+    private String carBase;
+
+    @Value("${api.reservation}")
+    private String reserveBase;
+    private String url;
+
     @GetMapping(value = { "/", "/index" })
-    public String index(Model model) {
+    public String index(Model model){
+        RestTemplate rt = new RestTemplate();
+        url = carBase + "car";
+        List<Car> cars = rt.getForObject(url, List.class);
+        model.addAttribute("cars", cars);
         return "index";
     }
-
 }
